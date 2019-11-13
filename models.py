ef2e77817588f0ae2d557e88e66d44c6aca9ac56
Rhys909 <rhyskconnor99@gmail.com>
2019-11-13 19:17:57 +0000

    Updated models.py to support tinyint datatype

--- models.py
+++ models.py
@@ -10,10 +10,10 @@ class PoliticalParty(db.Model):
 
 class Users(db.Model):
 	id = db.Column(db.Char(38), primary_key = True)
-	EligibleToVote = db.Column(db.Binary(1))
+	EligibleToVote = db.Column(db.TINYINT(1))
 	Email = db.Column(db.Varchar(255), nullable = False)
 	Password = db.Column(db.Varchar(255), nullable = False)
-	IsOfficial = db.Column(db.Binary(1))
+	IsOfficial = db.Column(db.TINYINT(1))
 	Votes = db.relationship('Vote', backref='user', lazy = True)
 
 	def __repr__(self):
@@ -23,8 +23,7 @@ class Vote(db.Model):
 	id = db.Column(db.Char(38), primary_key = True)
 	User_id = db.Column(db.Char(38), db.ForeignKey('user.id'), nullable = False)
 	Party_id = db.Column(db.Char(38), db.ForeignKey('party.id'), nullable = False)
-	VoteStatus = db.Column(db.Binary(1))
+	VoteStatus = db.Column(db.TINYINT(1))
 
 	def __repr__(self):
 		return f"Vote('{self.VoteStatus}')"
-
