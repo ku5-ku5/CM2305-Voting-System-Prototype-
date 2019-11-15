cff79ab0019d8e2f1359f963fac13cd9d7051ce9
Rhys909 <rhyskconnor99@gmail.com>
2019-11-15 11:15:40 +0000

    Added password hashing

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 import os
 from flask import render_template, url_for, request, redirect, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 from prototype import app
 
 @app.route("/")
@@ -7,8 +8,15 @@ from prototype import app
 def login():
 	return(render_template('login.html', title="Online Vote - Login"))
 
-@app.route("/register")
+@app.route("/register", methods=['GET','POST'])
 def register():
+	form = registrationForm()
+	if form.validate_on_submit():
+		password_hash = generate_password_hash(form.password.data, "sha256")
+		if check_password_hash(password_hash, form.password.data):
+			user = Users(Email=form.email.data, password=password_hash)
+		else:
+			flash("Password error please try again")
 	return(render_template('register.html', title="Online Vote - Register"))
 
 @app.route("/vote", methods=['GET','POST'])
