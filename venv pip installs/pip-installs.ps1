cd204a4f774a5816b516c4cbba952693ec8eb4dd
rhys909 <rhyskconnor99@gmail.com>
2019-11-22 13:35:45 +0000

    Added a Powershell script to setup your virtual environments and perform the pip installs

---
+++ venv pip installs/pip-installs.ps1
@@ -0,0 +1,52 @@
+﻿#May need to run Set-ExecutionPolicy Bypass -File ".\pip-installs.ps1"
+
+#May need to change the below to match your directory
+cd C:\repos\CM2305-Voting-System-Prototype-\
+
+CMD /C "py -m venv venv"
+
+CMD /C "venv\Scripts\activate"
+
+CMD /C "pip install mysql-connector-python"
+CMD /C "pip install sqlalchemy"
+CMD /C "pip install flask"
+CMD /C "pip install flask-wtf"
+CMD /C "pip install jinja2"
+CMD /C "pip install pymysql"
+CMD /C "pip install flask-login"
+CMD /C "pip install -U Werkzeug"
+CMD /C "pip install flask-sqlalchemy"
+CMD /C "pip install Faker"
+CMD /C "pip install "
+
+# SIG # Begin signature block
+# MIIFZwYJKoZIhvcNAQcCoIIFWDCCBVQCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUZo0UauB/wSvWPQmubRIWeZXK
+# HLqgggMEMIIDADCCAeigAwIBAgIQetzlqksqA41HXlytKYfndDANBgkqhkiG9w0B
+# AQUFADAYMRYwFAYDVQQDDA1SaHlzQ29ubm9yIFBTMB4XDTE5MTEyMTE5NTc0NVoX
+# DTIwMTEyMTIwMTc0NVowGDEWMBQGA1UEAwwNUmh5c0Nvbm5vciBQUzCCASIwDQYJ
+# KoZIhvcNAQEBBQADggEPADCCAQoCggEBALY0hjBdhU77MGMR9xHczkbRDIuJXGFX
+# XJ1BWPzfkouNjNXnN5+ShTPfcEA7/XT2JxFZxCR2brWFFIbvFMHRrBlBKn1LtJxt
+# muDbOV+sf9hZbA0GeUwkjBfd50Mby6Ssr4RfWv1CH3JmS07RPRccPnNyOOe+NLjV
+# bv+nZWExYc2ZtBIWB8zaxFugp1khWUZg/7fuxvBRsitMaPJh64ExYy4z7wbI+18M
+# JKVM0y9mp1YQsy8wvHtAIPaP2WXdfoAQPt/dSuTUT9eEGfOXH1lOmJQkxSHsakh5
+# ZlWgdpA7zpRo98ggMdzkx9GlLpigsM8njya9HXvfyFAm4aNPwS3SS/kCAwEAAaNG
+# MEQwDgYDVR0PAQH/BAQDAgeAMBMGA1UdJQQMMAoGCCsGAQUFBwMDMB0GA1UdDgQW
+# BBR/OBrhGngW7/RxtpYW2++pdGTahjANBgkqhkiG9w0BAQUFAAOCAQEAp4m0n/t9
+# WSuvXCGMtPnQwdwjjLTRpsPoMdM9jwAiUQALww1J/GdDgTW9+GQYC9IQ1ZhGiWP5
+# wssslR88bjASziWVeccVgSXY4aNXcahP+7JNvAgZK/T0yRBY0XJ7N/JH40vQWuux
+# gC37ZbbZXDX4xIdhFWpa/EA3gKAaN8dRuqnZzxyIkOEjUvcvgPI3HIcc6SWTlZN0
+# auaLmPh4Q4UD5BK4maOXTN4x2f9ywpTNzq4JnLdJvebBQdjgiEWZykuXNkuQhvDT
+# 6EjS9hQGyW4kb+Qu2Ub44AhA9peJvqyJxi0m84nRfTuMCJOH6nLQbQlkVEx+dkNN
+# dE8Pu+lTr8tXujGCAc0wggHJAgEBMCwwGDEWMBQGA1UEAwwNUmh5c0Nvbm5vciBQ
+# UwIQetzlqksqA41HXlytKYfndDAJBgUrDgMCGgUAoHgwGAYKKwYBBAGCNwIBDDEK
+# MAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYBBAGCNwIBBDAcBgorBgEEAYI3
+# AgELMQ4wDAYKKwYBBAGCNwIBFTAjBgkqhkiG9w0BCQQxFgQUYezWx/ANYPMDlOlu
+# a6/7Y/yvTTQwDQYJKoZIhvcNAQEBBQAEggEAXG0UzF7Cu/1KJhm/kLSSq6izHNwW
+# 7xYTCXqJh5XkU0ztHg1wgfP/FRWvd9aratHYUvxolOO/BwaB9T8Di8QVZeLntBiI
+# wceAhhPupE0kqOipv/qDi+RJpflXG1ZNRCRZes7/FvqyFfQa+8LkgIAO2Pb4LwV2
+# cA9Bdnl1NHWT1245o0e7UXmR3h2LjQ2aaHk+zrzMrI7Mzg54oJGQiPgqcpbHNVTp
+# T/O+58PTSHHA/SVjmvYSoWnCbazVBImiWYaLFpj0GJNFB5Jp2RyR5iUl7jgLhDeH
+# IMY9PeNKIoh9rVxvTRETBnyvDgA7saU/KgLS5C3H9IZYSVZxBHj+Q+bsAA==
+# SIG # End signature block
