c53eed588788cab7bbcdb9db6ce37852ba50b214
tomoshuw <44280020+tomoshuw@users.noreply.github.com>
2019-12-03 17:10:16 +0000

    Update __init__.py

--- Prototype/__init__.py
+++ Prototype/__init__.py
@@ -4,9 +4,9 @@ from flask_login import LoginManager
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '27a695e8e5c505c88f6de409f873e7a06543dca3c40c9cbb'
+app.config['SECRET_KEY'] = '34a695e8e5c505c88f6de409f873e7a06543cda3c40c9cbb'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Belgium2015@localhost:3306/votedb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:PASSWORD@localhost:3306/votedb'
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
