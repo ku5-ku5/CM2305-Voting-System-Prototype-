7beca1fa37a24a6a53bd02f1d42f21392f7a234c
rhys909 <rhyskconnor99@gmail.com>
2019-12-12 18:39:16 +0000

    Updated SQL scripts

--- SQL Scripts/Set Up Politicians Table.sql
+++ SQL Scripts/Set Up Politicians Table.sql
@@ -1,32 +1,3 @@
-CREATE DATABASE VoteDB;
-
-USE VoteDB;
-
-CREATE TABLE PoliticalParty(
- UId CHAR(38) NOT NULL UNIQUE,
- Name VARCHAR(255) NOT NULL,
- PRIMARY KEY (UId)
-);
-
-CREATE TABLE Vote(
-  VoteId CHAR(38) NOT NULL UNIQUE,
-  PoliticalPartyID CHAR(38) NOT NULL,
-  /**VoteStatus TINYINT(1) DEFAULT 0,**/
-  VoteTimestamp DATETIME NOT NULL,
-  PRIMARY KEY (VoteId),
-  FOREIGN KEY (PoliticalPartyID) REFERENCES PoliticalParty(UId)
-);
-
-
-CREATE TABLE Users(
-  UserUId CHAR(38) NOT NULL UNIQUE,
-  EligibleToVote TINYINT(1) DEFAULT 0,
-  Email VARCHAR(255) NOT NULL UNIQUE,
-  PwdHash VARCHAR(255),
-  HasVoted TINYINT(1) DEFAULT 0,
-  IsOfficial TINYINT(1) DEFAULT 0,
-  PRIMARY KEY (UserUId)
-);
 
 
 INSERT INTO `votedb`.`politicalparty`
--- SQL Scripts/Vote_Table.sql
+++ SQL Scripts/Vote_Table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE Vote(
-  UId CHAR(38), 
-  VoteID int IDENTITY(1,1), 
-  PoliticalPartyID CHAR(38) NOT NULL, 
-  VoteStatus BINARY(1) DEFAULT 0, 
-  PRIMARY KEY (UId), 
-  FOREIGN KEY (PoliticalPartyID) REFERENCES PoliticalParty(PartyID) 
-); 
+  UId CHAR(38),
+  VoteID int IDENTITY(1,1),
+  PoliticalPartyID CHAR(38) NOT NULL,
+  VoteTimestamp DATETIME NOT NULL,
+  PRIMARY KEY (UId),
+  FOREIGN KEY (PoliticalPartyID) REFERENCES PoliticalParty(PartyID)
+);
