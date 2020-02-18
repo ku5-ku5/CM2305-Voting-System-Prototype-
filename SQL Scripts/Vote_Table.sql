e06e0c7ead30808a27301bc47c368b781b0c737d
tomoshuw <44280020+tomoshuw@users.noreply.github.com>
2020-02-18 16:32:17 +0000

    Delete Vote_Table.sql

--- SQL Scripts/Vote_Table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Vote(
-  UId CHAR(38), 
-  VoteID int IDENTITY(1,1), 
-  PoliticalPartyID CHAR(38) NOT NULL, 
-  VoteStatus BINARY(1) DEFAULT 0, 
-  PRIMARY KEY (UId), 
-  FOREIGN KEY (PoliticalPartyID) REFERENCES PoliticalParty(PartyID) 
-); 
