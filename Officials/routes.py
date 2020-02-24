ec167f971d05d790d09ec3a068fad2159edf23ae
tomoshuw <tomoshuw@github.com>
2020-02-24 19:58:59 +0000

    updated officials reg

--- Officials/routes.py
+++ Officials/routes.py
@@ -1,5 +1,3 @@
-#/usr/bin/python3
-
 import os
 from flask import render_template, url_for, request, redirect, flash
 from sqlalchemy.orm import load_only
@@ -7,13 +5,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import hashlib
 from Officials import app, db
 from flask_login import login_user, current_user, logout_user, login_required
+from Officials.forms import Officials_Registration
 
 @app.route("/")
 @app.route("/index", methods=['GET', 'POST'])
 def index():
     return render_template('officials_home.html', title="Online Vote System")
-
-"""
+'''
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = loginForm()
@@ -27,48 +25,17 @@ def login():
             flash("Invalid username or password!")
             return redirect(url_for('login'))
     return render_template('login.html', title="Online Vote - Login",form=form)
-
+'''
 @app.route("/register", methods=['GET','POST'])
 def register():
-    form = registrationForm()
+    form = Officials_Registration()
     if request.method == 'POST':
         if form.validate_on_submit():
             password_hash = hashlib.sha256(form.password.data.encode()).hexdigest()
             user = Users(email=form.email.data, PwdHash=password_hash)
             db.session.add(user)
             db.session.commit()
-            return redirect(url_for('login'))
+            return redirect(url_for('index'))
         else:
             return flash("Unknown error please try again later")
-    return render_template('register.html', title="Online Vote - Register",form=form)
-
-@app.route("/vote", methods=['GET','POST'])
-def vote():
-    if current_user.is_authenticated:
-        if current_user.check_vote_eligibility() & current_user.check_has_voted():
-            form = SubmitVoteForm()
-            form.chosenParty.choices = [(PoliticalParty.UId, PoliticalParty.Name) for PoliticalParty in PoliticalParty.query.all()]
-            parties = PoliticalParty.query.all()
-            return render_template('vote.html', politicalparty=parties, title="Voting Page", form=form)
-        if request.method == 'POST':
-            vote = form.chosenParty.data
-            db.session.add(vote)
-            flash("Thank you for voting " + form.chosenParty.data)
-            return redirect(url_for('home'))
-        else:
-            return redirect(url_for('unauthorised'))
-    else:
-        flash("Please login to access this page")
-        return redirect(url_for('login'))
-
-@app.route("/unauthorised", methods=['GET','POST'])
-def unauthorised():
-    return render_template('unauthorised.html', title="Unauthorised")
-
-@app.route("/home", methods=['GET', 'POST'])
-def home():
-    if current_user.is_authenticated:
-        return render_template('home.html', title="User Home Page")
-    else:
-        return redirect(url_for('unauthorised'))
-"""
+    return render_template('officials_register.html', title="Register as an Official", form=form)
