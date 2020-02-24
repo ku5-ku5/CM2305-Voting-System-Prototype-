8141cb1499daca9f27f446355e73bc502fc960c8
tomoshuw <44280020+tomoshuw@users.noreply.github.com>
2020-02-24 16:26:23 +0000

    Update __init__.py

--- votingsystem/__init__.py
+++ votingsystem/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7a8c2ba043e7b74aa0da7d2c0009effb8f6f9b0c36c45eab'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Belgium2015@localhost:3306/votedb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:PASSWORD@localhost:3306/votedb'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
