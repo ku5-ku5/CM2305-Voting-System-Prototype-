0a507fb2f5e674778543f9f776cca7494f21cb37
tomoshuw <tomoshuw@github.com>
2020-02-25 13:56:54 +0000

    fixed some validation errors

--- Officials/forms.py
+++ Officials/forms.py
@@ -1,17 +1,16 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, RadioField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp, ValidationError
+from Officials.models import Official
 
 class Officials_Registration(FlaskForm):
-    first_name = StringField('First Name', validators=[DataRequired('Please enter your first name'), Length(min=3, max=50)])
-    last_name = StringField('Last Name', validators=[DataRequired('Please enter your last name'), Length(min=3, max=50)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Regexp('^.{6,20}$', message='Your password needs to be longer than 6 characters')])
     confirm_password = PasswordField('Confirm Your Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
     submit = SubmitField('Sign Up')
 
-    def validEmail(self, email):
-        mail = User.query.filter_by(email=email.data).first()
+    def validate_email(self, email):
+        mail = Official.query.filter_by(email=email.data).first()
         if mail:
             raise ValidationError('This email is already being used.')
 
--- Officials/routes.py
+++ Officials/routes.py
@@ -1,5 +1,5 @@
 import os
-from flask import render_template, url_for, request, redirect, flash
+from flask import render_template, url_for, request, redirect, flash, session
 from sqlalchemy.orm import load_only
 from werkzeug.security import generate_password_hash, check_password_hash
 import hashlib
@@ -8,7 +8,9 @@ from flask_login import login_user, current_user, logout_user, login_required
 from Officials.forms import Officials_Registration, loginForm
 from Officials.models import Official
 
+
 @app.route("/")
+@app.route("/home")
 @app.route("/index", methods=['GET', 'POST'])
 def index():
     return render_template('officials_home.html', title="Online Vote System")
@@ -16,27 +18,29 @@ def index():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = loginForm()
-    if request.method == 'POST':
+    if form.validate_on_submit():
         user = Official.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(hashlib.sha256(form.password.data.encode()).hexdigest()):
             login_user(user)
-            flash("Login Successful")
+            flash("Login Successful", 'success')
             return redirect(url_for('index'))
         else:
-            flash("Invalid username or password!")
+            flash("Invalid username or password!", 'danger')
             return redirect(url_for('login'))
     return render_template('officials_login.html', title="Online Vote - Login",form=form)
 
 @app.route("/register", methods=['GET','POST'])
 def register():
     form = Officials_Registration()
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            password_hash = hashlib.sha256(form.password.data.encode()).hexdigest()
-            user = Official(email=form.email.data, password=password_hash)
-            db.session.add(user)
-            db.session.commit()
-            return redirect(url_for('login'))
-        else:
-            return flash("Unknown error please try again later")
+    if form.validate_on_submit():
+        password_hash = hashlib.sha256(form.password.data.encode()).hexdigest()
+        user = Official(email=form.email.data, password=password_hash)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
     return render_template('officials_register.html', title="Register as an Official", form=form)
+
+@app.route("/logout")
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
