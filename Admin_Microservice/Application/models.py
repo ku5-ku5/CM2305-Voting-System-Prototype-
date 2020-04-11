8cddbab8cc9508fcf99b1550a669f073cfd504f7
rhys909 <connorr@cardiff.ac.uk>
2020-04-11 16:59:09 +0100

    Added some comments and fixed officials models.py

--- Admin_Microservice/Application/models.py
+++ Admin_Microservice/Application/models.py
@@ -9,18 +9,28 @@ def load_user(user_id):
 	return Official.query.get(int(user_id))
 
 class Official(db.Model, UserMixin):
-		id = db.Column(db.Integer, primary_key=True)
-		email = db.Column(db.String(240), unique=True, nullable=False)
-		password = db.Column(db.String(120), nullable=False)
+	OfficialUid = db.Column(db.UUID, primary_key=True)
+	FirstName = db.Column(db.String(50), nullable=False)
+	Surname = db.Column(db.String(50),nullable=False)
+	Email = db.Column(db.String(240), unique=True, nullable=False)
+	PwdHash = db.Column(db.String(255), nullable=False)
+	IsAdmin = db.Column(db.TINYINT, default=0, nullable=False)
 
-		def __repr__(self):
-			return f"User('{self.email}')"
+	def get_id(self):
+		try:
+			return str(self.OfficialUid)
+		except AttributeError:
+			raise NotImplementedError('No `UId` attribute - override `get_id`')
 
-		def verify_password(self, password):
-			return self.password == password
+	def __repr__(self):
+		return f"User('{self.email}')"
+
+	def verify_password(self, password):
+		return self.PwdHash == password
 
 class Election(db.Model, UserMixin):
-	title = db.Column(db.String(255), nullable=False, primary_key=True)
+	Id = db.Column(db.Integer, nullable=False, primary_key=True)
+	Name = db.Column(db.String(255), nullable=False)
 	election_date = db.Column(db.DATE, nullable=False)
 	start_time = db.Column(db.TIME, nullable=False)
 	end_time = db.Column(db.TIME, nullable=False)
--- Admin_Microservice/Application/routes.py
+++ Admin_Microservice/Application/routes.py
@@ -4,7 +4,6 @@ from sqlalchemy.orm import load_only
 from werkzeug.security import generate_password_hash, check_password_hash
 import hashlib
 from Application import app, db
-
 from flask_login import login_user, current_user, logout_user, login_required
 from Application.forms import Officials_Registration, loginForm, CreateElectionForm
 from Application.models import Official, Election, Candidates
@@ -17,14 +16,19 @@ import numpy as np
 @app.route("/home")
 @app.route("/index", methods=['GET', 'POST'])
 def index():
+    #Renders the template for the officials and admin homepage
     return render_template('officials_home.html', title="Online Vote System")
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    #initiates login form
     form = loginForm()
     if form.validate_on_submit():
+        #gets the data from the form
         user = Official.query.filter_by(email=form.email.data).first()
+        #checks that the user exists and that the password is correct 
         if user is not None and user.verify_password(hashlib.sha256(form.password.data.encode()).hexdigest()):
+            #uses the flask login and logs in the user
             login_user(user)
             flash("Login Successful", 'success')
             return redirect(url_for('index'))
@@ -34,27 +38,37 @@ def login():
     return render_template('officials_login.html', title="Online Vote - Login",form=form)
 
 @app.route("/register", methods=['GET','POST'])
+#In a real working system this wouldn't exist
 def register():
+    #initiates the registration form 
     form = Officials_Registration()
     if form.validate_on_submit():
+        #gets the data from the form 
+        #hashes the password in a sha256 hash
         password_hash = hashlib.sha256(form.password.data.encode()).hexdigest()
         user = Official(email=form.email.data, password=password_hash)
+        #pushes the user data to the database
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('login'))
     return render_template('officials_register.html', title="Register as an Official", form=form)
 
+#page for admins to create an election
 @app.route("/create_election", methods=['GET', 'POST'])
 def create_election():
+    #initiates the create election form
     form = CreateElectionForm()
     if form.validate_on_submit():
+        #Takes the data from the form and creates the new election as an Election object
         new_election = Election(title=form.title.data, election_date=form.election_date.data, start_time=form.start_time.data, end_time=form.end_time.data)
+        #adds the new election to the database
         db.session.add(new_election)
         db.session.commit()
         flash("Successfully Created Election!", "success")
         return redirect(url_for('index'))
     return render_template("create_election.html", title="Create Election", form=form)
 
+#A page for admins and officials to view elections 
 @app.route('/view_election')
 def view_election():
     connection = db.engine.raw_connection()
@@ -65,11 +79,13 @@ def view_election():
 
 @app.route("/logout")
 def logout():
+    #using flask login user management this page logs out the user
 	logout_user()
 	return redirect(url_for('index'))
 
 @app.route("/results", methods=['GET', 'POST'])
 def results():
+    #page that gets all the vote data for the current election
     votes = Vote.query.all()
     parties = PoliticalParty.query.all()
     results = {}
